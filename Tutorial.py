53fe10b778e2162f8867df4eb187ab7762efedb8
ElliotMG <elliotgray79@gmail.com>
2026-03-11 17:07:36 +0000

    more fiddling with Tutorial.py - good to go now

--- Tutorial.py
+++ Tutorial.py
@@ -1,7 +1,9 @@
+# Import packages
 import numpy as np
 import xarray as xr
 import warnings
 import os,sys
+import datetime as dt
 import intake
 import healpy
 from lossett.calc.calc_inter_scale_transfers import calc_inter_scale_energy_transfer_kinetic
@@ -12,6 +14,7 @@ warnings.filterwarnings(
     category=FutureWarning,
 )
 
+# HEALPix to lat lon conversion function from easy.gems
 def get_nn_lon_lat_index(nside, lons, lats):
     lons2, lats2 = np.meshgrid(lons, lats)
     return xr.DataArray(
@@ -19,12 +22,14 @@ def get_nn_lon_lat_index(nside, lons, lats):
         coords=[("latitude", lats), ("longitude", lons)],
     )
 
+# Read in ERA5 data from DKRZ mirror
 cat = intake.open_catalog('https://digital-earths-global-hackathon.github.io/catalog/catalog.yaml')['online']
 
 sim = cat['ERA5']
-zoom=7
+zoom = 7
 ds = sim(zoom=zoom).to_dask() # Zoom=6 is about 1 deg
 
+# Supersampling for smoothness in the conversion and interpolating to 1x1deg grid
 supersampling = {"longitude": 4, "latitude": 4}
 
 idx = get_nn_lon_lat_index(
@@ -33,24 +38,26 @@ idx = get_nn_lon_lat_index(
     np.linspace(-90, 90, supersampling['latitude']*90)
 )
 
-y = int(sys.argv[1])
-m = int(sys.argv[2])
-d = int(sys.argv[3])
+# Choose date for the example in the command line (run program like python Tutorial.py 2016 08 01 for one day for example)
+year = int(sys.argv[1])
+month = int(sys.argv[2])
+day = int(sys.argv[3])
 
-# In this example we're just doing 2D on each layer to save having to convert ERA5 omega to w
-u_lon_lat = ds.u.isel(cell=idx).sel(time=slice(dt.datetime(2016,8,1),dt.datetime(2016,8,1))).coarsen(supersampling).mean()
-v_lon_lat = ds.v.isel(cell=idx).sel(time=slice(dt.datetime(2016,8,1),dt.datetime(2016,8,1))).coarsen(supersampling).mean()
+# In this example we're just doing 2D on each layer to save having to convert ERA5 omega to w. Creating u,v,w arrays
+u_lon_lat = ds.u.isel(cell=idx).sel(time=slice(dt.datetime(year,month,day),dt.datetime(year,month,day))).coarsen(supersampling).mean()
+v_lon_lat = ds.v.isel(cell=idx).sel(time=slice(dt.datetime(year,month,day),dt.datetime(year,month,day))).coarsen(supersampling).mean()
 w_lon_lat = xr.zeros_like(u_lon_lat)
 w_lon_lat.name='w'
 # omega_lon_lat = ds.w.isel(time=slice(0,1), cell=idx).coarsen(supersampling).mean()
 
+# Merge u,v,w arrays into one dataset. The ds_u_3D that LoSSETT looks for.
 ds_u_3D = xr.merge([u_lon_lat, v_lon_lat, w_lon_lat])
 ds_u_3D = ds_u_3D.rename({'level':'pressure'})
-plevs = [10,70,100,150,200,250,300,400,500,600,700,850,925]
+plevs = [10,70,100,150,200,250,300,400,500,600,700,850,925] # Selection of plevs with high resolution near the UTLS.
 ds_u_3D = ds_u_3D.sel(pressure=plevs).compute()
 
 control_dict = {
-    "max_r": 10.0,
+    "max_r": 10.0, # Max radius in degrees we want our kernel to get to
     "max_r_units": "deg",
     "angle_precision": 1e-10,
     "x_coord_name": "longitude",
@@ -61,12 +68,13 @@ control_dict = {
     "y_coord_boundary": np.nan
 }
 
+# Select length scales of interest
 length_scales = np.array(
     [110,220,330,500,640,800,1000,1250,1600,2000,2500,3200,4000,5000]
 )
 length_scales = 1000.0 * length_scales # convert to m; ensure float
 
-# calculate kinetic DR indicator
+# calculate kinetic DR indicator -- this runs the LoSSETT calculation
 Dl_u = calc_inter_scale_energy_transfer_kinetic(
     ds_u_3D, control_dict,
     length_scales=length_scales
@@ -77,11 +85,14 @@ Dl_u = Dl_u.transpose("length_scale","time","pressure","latitude","longitude")
 
 model = 'ERA5'
 var = 'inter_scale_transfer_of_kinetic_energy'
-L_min = length_scales[0]
-L_max = length_scales[-1]
-dt_str = f"{datetime.y:04d}{datetime.m:02d}{datetime.d:02d}"
+L_min = length_scales[0] / 1000
+L_max = length_scales[-1] / 1000
 
-fname = f"{model}_{var}_Lmin_{L_min:05.0f}_Lmax_{L_max:05.0f}_{dt_str}.nc"
+datetime = dt.datetime(year,month,day)
+dt_str = f"{datetime.year:04d}{datetime.month:02d}{datetime.day:02d}"
 
+fname = f"{model}_{var}_Lmin_{L_min:05.0f}km_Lmax_{L_max:05.0f}km_{dt_str}.nc"
+
+# Write file to output where you can now play around with visualising. See the notebook for instructions on how to navigate the output.
 Dl_u.to_netcdf(fname)
 print(f'File {fname} saved')
